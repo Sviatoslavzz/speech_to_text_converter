c73f47a8ce554fa024512111e55352c69f27ddbf
Sviatoslav <sviatoslav@Sviatoslavs-MacBook-Pro.local>
2024-07-12 11:19:34 +0300

    [feature] changed youtube downloader module for yt_dlp, multiprocessing added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import whisper
-from loader import Loader
+# from pytube_loader import Loader
+from yt_dlp_loader import Yt_loader
 import time
 import warnings
+import os
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using FP32 instead")
 
@@ -17,26 +20,65 @@ def collect_links() -> list:
     return links
 
 
+def transcribe(directory: str, link: str, model):
+    loader = Yt_loader(link)
+    title, is_loaded = loader.download_audio()
+    if is_loaded:
+        result = model.transcribe(title)
+        if title.endswith(".mp3"):
+            title = title.replace(".mp3", ".txt")
+            with open(f'{directory}/{title}', "w") as file:
+                file.write(result['text'])
+            print(f"Transcription saved\ntitle: {title}\n")
+        else:
+            print("Please check audio extension")
+
+
 def main():
+    directory = 'saved_files'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     links = collect_links()
     print("starting loop...")
-    for i, link in enumerate(links, start=1):
-        load = Loader(link)
-        title = load.download_audio()
+    start_t = time.time()
+    model = whisper.load_model("base")
 
-        start_t = time.time()
-        print(f"whisper started {i}/{len(links)}...")
-        model = whisper.load_model("base")
-        result = model.transcribe(title)
+    max_workers = 4
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        link_iter = iter(links)
 
-        if title.endswith(".mp4"):
-            title = title.replace(".mp4", ".txt")
+        for _ in range(max_workers):
+            try:
+                link = next(link_iter)
+                futures.append(executor.submit(transcribe, directory, link, model))
+            except StopIteration:
+                break
 
-        with open(title, "w") as file:
-            file.write(result['text'])
+        completed_count = 0
+        while futures:
+            for future in as_completed(futures):
+                futures.remove(future)
+                try:
+                    future.result()  # This will raise any exceptions encountered in transcribe
+                    completed_count += 1
+                    print(f"whisper completed {completed_count}/{len(links)}")
+                except Exception as e:
+                    print(f"Error processing link: {e}")
 
-        print(f"total time for whisper transcription: {round(time.time() - start_t, 2)} sec.")
-        print(f"Transcription saved\ntitle: {title}\n")
+                # Submit a new task if there are more links to process
+                try:
+                    link = next(link_iter)
+                    futures.append(executor.submit(transcribe, directory, link, model))
+                except StopIteration:
+                    continue
+
+    # for i, link in enumerate(links, start=1):
+    #     print(f"whisper started {i}/{len(links)}...")
+    #     transcribe(directory, link, model)
+
+    print(f"total time : {round(time.time() - start_t, 2)} sec.")
 
 
 if __name__ == "__main__":
